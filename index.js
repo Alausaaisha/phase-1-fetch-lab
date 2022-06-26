bc8a64cc4d190245d5c4a2b5158d114c619c6fbc
Alausa Aishat <qtaisha.ao@gmail.com>
2022-06-26 10:32:26 +0100

    fetch lab

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then(GameofThronesData => renderBooks(GameofThronesData))
   
 }
 
@@ -7,8 +10,13 @@ function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
+    const div = document.createElement('div');
+    const p = document.createElement('p');
+
     h2.innerHTML = book.name;
     main.appendChild(h2);
+    main.appendChild(div);
+    div.appendChild(p);
   });
 }
 
